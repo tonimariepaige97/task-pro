d528e4d19b665849261391ac4513177de6c63c3e
tonimariepaige97 <tonimariepaige97@gmail.com>
2025-02-06 16:07:11 -0700

    Create an event listener for the create task button

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const createTaskBtn = document.querySelector(".create-task-btn");
 const powerModeBtn = document.querySelector(".power-mode-btn");
 const creationOfTask = document.querySelector(".right-section-of-screen");
+
+// EVENT LISTENERS
+createTaskBtn.addEventListener("click", function () {
+  console.log("clicked");
+});
