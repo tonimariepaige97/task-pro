58ae31c5aea734ef8ebcd608e604e819c36a575f
tonimariepaige97 <tonimariepaige97@gmail.com>
2025-02-10 12:15:22 -0700

    Create the display for the create task section

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const createTaskBtn = document.querySelector(".create-task-btn");
 const powerModeBtn = document.querySelector(".power-mode-btn");
 const creationOfTask = document.querySelector(".right-section-of-screen");
 
+function displayCreateTaskSection() {
+  // Create header for create task section
+  const createTaskHeader = document.createElement("p");
+  createTaskHeader.classList.add("create-task-header");
+  createTaskHeader.textContent = "Create a Task";
+  creationOfTask.append(createTaskHeader);
+
+  // Create background for create task section
+  const createTaskDiv = document.createElement("div");
+  createTaskDiv.classList.add("create-task-div");
+  creationOfTask.append(createTaskDiv);
+}
+
 // EVENT LISTENERS
 createTaskBtn.addEventListener("click", function () {
-  creationOfTask.innerHTML = "";
+  creationOfTask.innerText = "";
+  displayCreateTaskSection();
 });
