26345c7df059a357e7598d0b8074234434f7acc3
tonimariepaige97 <tonimariepaige97@gmail.com>
2025-02-13 15:54:40 -0700

    Code refactored

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-const createTaskBtn = document.querySelector(".create-task-btn");
-const powerModeBtn = document.querySelector(".power-mode-btn");
-const creationOfTask = document.querySelector(".right-section-of-screen");
+const rightContentSection = document.querySelector(".right-content-section");
+const wholeApp = document.querySelector(".whole-app");
 
-function displayCreateTaskSection() {
-  // Create header for create task section
-  const createTaskHeader = document.createElement("p");
-  createTaskHeader.classList.add("create-task-header");
-  createTaskHeader.textContent = "Create a Task";
-  creationOfTask.append(createTaskHeader);
+function displayAllTasksPage() {
+  // create an all tasks page for the task page
+  const allTasksPage = document.createElement("div");
+  allTasksPage.classList.add("all-tasks-page");
+  rightContentSection.append(allTasksPage);
 
-  // Create background for create task section
-  const createTaskDiv = document.createElement("div");
-  createTaskDiv.classList.add("create-task-div");
-  creationOfTask.append(createTaskDiv);
+  // create the container for the search bar display
+  const searchBarContainer = document.createElement("div");
+  searchBarContainer.classList.add("search-bar-container");
+  allTasksPage.append(searchBarContainer);
+
+  // create the input for the search bar
+  const searchBarDisplay = document.createElement("input");
+  searchBarDisplay.classList.add("search-bar-display");
+  searchBarDisplay.setAttribute("type", "text");
+  searchBarDisplay.setAttribute("Placeholder", "search");
+  allTasksPage.append(searchBarDisplay);
+
+  // create the task container
+  const createTaskContainer = document.createElement("div");
+  createTaskContainer.classList.add("create-task-container");
+  allTasksPage.append(createTaskContainer);
+
+  // create the task section
+  const createTaskSection = document.createElement("div");
+  createTaskSection.classList.add("create-task-section");
+  createTaskContainer.append(createTaskSection);
+
+  // create all tasks p element
+  const allTasksPElement = document.createElement("p");
+  allTasksPElement.classList.add("all-tasks-p-element");
+  allTasksPElement.textContent = "All Tasks";
+  createTaskSection.append(allTasksPElement);
+
+  // create a container for the buttons
+  const buttonsContainer = document.createElement("div");
+  buttonsContainer.classList.add("buttons-container");
+  createTaskSection.append(buttonsContainer);
+
+  // create add task button
+  const createTaskBtn = document.createElement("button");
+  createTaskBtn.classList.add("create-task-button");
+  createTaskBtn.textContent = "+ Create Task";
+  buttonsContainer.append(createTaskBtn);
+
+  // create power mode button
+  const powerModeButton = document.createElement("button");
+  powerModeButton.classList.add("power-mode-button");
+  powerModeButton.textContent = "Power Mode";
+  buttonsContainer.append(powerModeButton);
 }
 
-// EVENT LISTENERS
-createTaskBtn.addEventListener("click", function () {
-  creationOfTask.innerText = "";
-  displayCreateTaskSection();
-});
+displayAllTasksPage();
+
+// // EVENT LISTENERS
+// createTaskBtn.addEventListener("click", function () {
+//   creationOfTask.innerText = "";
+//   displayCreateTaskSection();
+// });
